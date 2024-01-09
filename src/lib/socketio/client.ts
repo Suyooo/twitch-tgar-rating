00fd7151510df975cc6a185b3246f011c644d046
Suyooo <me@suyo.be>
2024-01-10 00:10:06 +0100

    Allow setting a different Socket.IO URL

--- src/lib/socketio/client.ts
+++ src/lib/socketio/client.ts
@@ -1,9 +1,12 @@
+import { env } from "$env/dynamic/public";
 import Client from "socket.io-client";
 
 export type CallbackResponse<T> = (T & { error: false }) | { error: true };
 
 export function createSocket(roomCode: string, isControl: boolean) {
 	console.log("Connecting...");
-	const socket = Client(":3001", { query: { roomCode, isControl: isControl ? "y" : "n" } });
+	const socket = Client(env.PUBLIC_SOCKETIO_URL ?? ":3001", {
+		query: { roomCode, isControl: isControl ? "y" : "n" },
+	});
 	return socket;
 }
